554be551ed2be0182fcdc782a97c36f9bb636434
jiimms <jiimms@yahoo.com>
2016-02-29 15:39:14 +0530

    Ruby2.2

---
+++ Ruby_S2_A2_diff_put_print.rb
@@ -0,0 +1,15 @@
+	
+		puts "The primary difference between puts and print is that puts adds a newline after executing,
+		 and print does not."
+		
+		
+		puts "Output with puts"
+		puts "Jimmy Mannan"
+		puts "Jimmy Mannan"
+		"\n"
+		puts "Output with print"
+		print "jimmy Mannan"
+		print "jimmy Mannan"
+
+
+	
